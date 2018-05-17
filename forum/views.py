01508e0aa21f83d7a03a8fb79d730f9617733b52
nedondodongo <willirmler@me.com>
2018-05-16 20:14:36 -0500

    Can now add new topic to each board w/validation

--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import Board
-
-# Create your views here.
+from .forms import NewTopicForm
+from .models import Board, Post, Topic
 
 def home(request):
     return render(request, 'home.html')
@@ -17,3 +16,25 @@ def forum_topics(request, pk):
     # Get board defined by primary key
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'forum_topics.html', {'board': board})
+
+def new_topic(request, pk):
+    # Get board defined by primary key
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # TODO: get current user instead
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )        
+            return redirect('forum_topics', pk=board.pk) # TODO: redirect to created topic
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board':board, 'form':form})
--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -22,6 +22,7 @@ from forum import views
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^forum/$', views.forum, name='forum'),
-    re_path(r'^forum/(?P<pk>\d+)/$', views.forum_topics, name='forum_topics'),        
+    re_path(r'^forum/(?P<pk>\d+)/$', views.forum_topics, name='forum_topics'),
+    re_path(r'^forum/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/', admin.site.urls),
 ]
